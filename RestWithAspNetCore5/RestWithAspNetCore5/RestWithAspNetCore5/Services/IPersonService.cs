74ca20cebf96f55eda5663a6a67766564aab5b2d
André Silva <andresilva428@gmail.com>
2021-03-09 19:09:42 -0300

    Ajusta controller para contexto de person

---
+++ RestWithAspNetCore5/RestWithAspNetCore5/RestWithAspNetCore5/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithAspNetCore5.Model;
+using System.Collections.Generic;
+
+namespace RestWithAspNetCore5.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
